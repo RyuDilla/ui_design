355c00f5b67c26af6b99f16bde00951db54f2aa0
KakugawariDaisuki221 <kakugawari.daisuki@gmail.com>
2019-05-12 18:42:31 +0900

    add StageSelect

---
+++ Assets/StageSelectController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelectController : MonoBehaviour
+{
+    public int selectedStageID;
+    private Image stageImage;
+    public List<Sprite> stageSpriteList;
+
+    private string[] sceneArray = {"Stage1", "Stage2", "Stage3"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject childObject = gameObject.transform.Find("StageImage").gameObject;
+        stageImage = childObject.GetComponent<Image>();
+    }
+
+    public void SetStageID(int stageID)
+    {
+        this.selectedStageID = stageID;
+        stageImage.sprite = stageSpriteList[stageID];
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneArray[selectedStageID]);
+    }
+}
