631f93477f7da0590f65c30e38a00504b35b30f2
KakugawariDaisuki221 <kakugawari.daisuki@gmail.com>
2019-05-16 23:37:36 +0900

    オプション画面　なんちゃってフラットデザイン

---
+++ Assets/SliderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public Slider userSlider;
+    public Text sliderValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderValue.text = string.Format("{0:F2}", userSlider.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnValueChanged()
+    {
+        sliderValue.text = string.Format("{0:F2}", userSlider.value);
+    }
+}
